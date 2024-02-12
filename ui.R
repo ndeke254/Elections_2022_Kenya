e56b64f02c788e292f016f9133d160ca4a1f6e6f
ndeke254 <musumbijefferson@gmail.com>
2024-02-12 16:20:24 +0300

    window title icon + flipdown + socials

--- ui.R
+++ ui.R
@@ -1,5 +1,10 @@
 # application user interface
 ui <- navbarPage(
+  windowTitle = tags$head(
+    tags$link(rel = "icon",
+              type = "image/png",
+              href = "shape.png"),
+    tags$title("Election Results 2022")),
   tags$audio(src = "sound.mp3", type = "audio/mp3",
              autoplay = TRUE, loop = TRUE),
   tags$style(HTML("
@@ -35,10 +40,104 @@ position: absolute;
  }
 
 ")),
-  windowTitle = "Election Results 2022",
   bsTooltip("check", "Extract county", "right", "hover"),
+  bsTooltip("twitter", "Twitter", "bottom", "hover"),
+  bsTooltip("github", "Github", "bottom", "hover"),
+  bsTooltip("linkedin", "LinkedIn", "bottom", "hover"),
   header = tagList(
     useShinydashboard(),
+    absolutePanel(
+      id = "timer",
+      class = "panel panel-default",
+      top = 10, 
+      left = "auto", 
+      right = "auto",
+      bottom ="auto",
+      width = "auto", 
+      height = "auto",
+      tags$div(
+        class = "p2",
+        tags$div(
+          class = "p3",
+          tags$div(
+            class = "flag",
+          img(
+            src = "kenya.png"
+          )
+          ),
+          tags$div(
+            "KENYA DECIDES 2027"
+          )
+        ),
+        tags$div(
+          class = "p6",
+          flipdownr::flipdown(
+            downto = "2024-04-30 00:00:00 EAT",
+            id = "flipdown", 
+            theme = "youkous"
+            )
+          )
+        )
+      ),
+    absolutePanel(
+      id = "socials",
+      class = "panel panel-default",
+      top = 10,
+      left = "auto",
+      right = 60,
+      bottom ="auto",
+      width = 140,
+      height = "auto",
+      tags$div(class = "p2",
+               tags$div(
+                 class = "p3",
+                 tags$div(
+                   class = "flag",
+                 tags$img(src = "author.png"),
+                 ),
+                 tags$div(
+                   "FIND ME"
+                   )
+               ),
+               tags$div(
+                 class = "p7",
+                 tags$li(
+                   tags$div(
+                     class = "p8",
+                     actionLink(
+                       "linkedin",
+                       label = "",
+                       icon = icon("linkedin"),
+                       onclick = "window.open('https://www.linkedin.com/in/jefferson-ndeke-027062202/')"
+                       )
+                     )
+                   ),
+                 tags$li(
+                   tags$div(
+                     class = "p8",
+                     actionLink(
+                       "github",
+                       label = "",
+                       icon = icon("github"),
+                       onclick = "window.open('https://github.com/ndeke254')")
+                     )
+                   ),
+                 tags$li(
+                   tags$div(
+                     class = "p8",
+                     actionLink(
+                       "twitter",
+                       label = "",
+                       icon = icon("twitter"),
+                       onclick = "window.open('https://twitter.com/jefferson_ndeke')"
+                       )
+                     )
+                   )
+                 ),
+               tags$a(href = "https://www.iebc.or.ke/resources/", "Data: IEBC",
+                      target = "_blank")
+               )
+      ),
     tags$div(
       id = "extract",
       Toggle.shinyInput("check", value = FALSE)
@@ -68,6 +167,7 @@ position: absolute;
       ))
   ),
   tabPanel(
+    
     #create a select input
     selectizeInput(
       inputId = "search_name",
